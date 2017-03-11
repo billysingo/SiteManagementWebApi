26e77077a1b71c4bc32473523519515dde89207d
billysingo <weekendy963@gmail.com>
2017-03-11 23:12:24 +0800

    add new features for editing attributes for sites.

--- server/public/controllers/controller.js
+++ server/public/controllers/controller.js
@@ -2,10 +2,14 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
     console.log('from the controller');
     $scope.isEditing = false;
+    $scope.isInfo = false;
+    $scope.detailEditing = {}
+
     var refresh = function () {
         $http.get('/sitelist').then(function (response) {
             console.log('i got the data', response);
             $scope.sites = response.data;
+            if (!$scope.isInfo)
             $scope.site = undefined;
         });
     };
@@ -42,6 +46,9 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
 
     $scope.update = function () {
         console.log($scope.site._id);
+        for (item in $scope.detailEditing){
+            $scope.detailEditing[item] = false;
+        }
         if (isNaN($scope.site.latitude) || isNaN($scope.site.longitude)) {
             alert('Error');
             refresh();
@@ -53,8 +60,43 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
         }
     };
 
+    $scope.info = function (id) {
+        console.log('inDetialMode');
+        $http.get('/sitelist/' + id).then(function (response) {
+            $scope.site = response.data;
+            $scope.isInfo = true;
+        })
+    }
+
+
     $scope.deselect = function () {
         $scope.site = undefined;
         $scope.isEditing = false;
     }
+
+    $scope.returnToHome = function () {
+        $scope.isInfo = false;
+        $scope.site = undefined;
+    }
+
+    $scope.addNewAttribute = function () {
+        if (!isNaN($scope.newAttrName) || ($scope.newAttrName in $scope.site)){
+            alert('参数名无效');
+            $scope.newAttrName = '';
+        }
+        else{
+        $scope.site[$scope.newAttrName] = $scope.newAttrValue;
+        $scope.newAttrName = '';
+        $scope.newAttrValue = '';
+       }
+    }
+
+    $scope.delAttribute = function (name) {
+            delete $scope.site[name];
+    }
+
+}]);
+
+myApp.controller('MessageCtrl',['$scope', '$http',function ($scope, $http) {
+    
 }]);
--- server/server.js
+++ server/server.js
@@ -15,12 +15,41 @@ app.get('/sitelist', function (req, res) {
     })
 });
 
+// app.post('/sitelist/:id', function (req, res) {
+//     var id = req.params.id;
+    
+//     var newObj = {};
+//     newObj[req.body.name] = req.body.value;
+//     console.log(newObj + ' : add attr');
+//     db.sites.findAndModify({
+//         query: {_id :mongojs.ObjectId(id)},
+//         update: {$set: newObj},
+//         new: true
+//     },function (err, doc) {
+//         res.json(doc);
+//     });
+// });
+
 app.post('/sitelist', function (req, res) {
     console.log(req.body);
     db.sites.insert(req.body, function (err, doc) {
         res.json(doc);
     });
 });
+
+// app.delete('/sitelist/:id/:name', function (req, res) {
+//     var id = req.params.id;
+//     var name = req.params.name
+//     var del = {};
+//     del[name] = 1;
+//     db.sites.findAndModify({
+//         query: {_id :mongojs.ObjectId(id)},
+//         update: {$unset: del},
+//         new: true
+//     },function (err,doc) {
+//         res.json(doc);
+//     });
+// });
 
 app.delete('/sitelist/:id', function (req, res) {
     var id = req.params.id;
@@ -39,14 +68,38 @@ app.get('/sitelist/:id', function (req, res) {
 
 app.put('/sitelist/:id', function (req, res) {
     var id = req.params.id;
-    console.log(req.body.name);
+    var del = {};
+    var setUpdate = {};
+    for (item in req.body){
+        if (item!='_id')
+            setUpdate[item] = req.body[item];
+    };
+    db.sites.findOne({_id: mongojs.ObjectID(id)}, function (err, doc) {
+        for (item in doc){
+            if (!(item in req.body)){
+                var del = {};
+                del[item] = 1;
+                console.log("删除内容: ",del);
+                db.sites.findAndModify({
+                    query: {_id :mongojs.ObjectId(id)},
+                    update: {$unset: del},
+                    new: true
+                },function (err,doc) {});
+            }
+        }
+    });
+    console.log('更新内容: ',setUpdate);
     db.sites.findAndModify({
         query: {_id: mongojs.ObjectID(id)},
-        update: {$set: {name: req.body.name, latitude: req.body.latitude, longitude: req.body.longitude}},
+        update: {$set: setUpdate},
         new: true
     }, function (err, doc) {
         res.json(doc);
     });
+
+
 });
+
+
 app.listen(3000);
 console.log("server listening at 3000");
